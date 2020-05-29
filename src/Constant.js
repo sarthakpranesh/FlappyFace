d7c3b18ea85c295af0d8c0ac78ddcee25ccd87c8
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-05-30 00:06:46 +0530

    Dropped matter-js with custom physics

--- src/Constant.js
+++ src/Constant.js
@@ -3,10 +3,12 @@ import {Dimensions} from 'react-native';
 const Constants = {
   MAX_WIDTH: Dimensions.get('window').width,
   MAX_HEIGHT: Dimensions.get('window').height,
-  GAP_SIZE: 200,
+  GAP_SIZE: 160,
+  PIPE_MIN: 200,
   PIPE_WIDTH: 60,
   BIRD_WIDTH: 34,
   BIRD_HEIGHT: 24,
+  FLOOR_HEIGHT: 30,
 };
 
 export default Constants;
