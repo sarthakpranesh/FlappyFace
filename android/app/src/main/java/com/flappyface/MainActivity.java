82c607b66008fe5e55b2598df71905a6dbbfd706
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2021-03-06 21:30:13 +0530

    Updated the whole project, now using TS, some libraries will be configured later
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- android/app/src/main/java/com/flappyface/MainActivity.java
+++ android/app/src/main/java/com/flappyface/MainActivity.java
@@ -1,8 +1,6 @@
-package com.flappybird;
+package com.flappyface;
 
-import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -10,15 +8,8 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-      SplashScreen.show(this);  // here
-      super.onCreate(savedInstanceState);
-  }
-
   @Override
   protected String getMainComponentName() {
-    return "flappybird";
+    return "FlappyFace";
   }
 }
